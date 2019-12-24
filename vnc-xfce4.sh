e4ccf45d2d7e3ad052badc00062acf78b9edc321
SADIK KUZU <sadikkuzu@hotmail.com>
2019-12-24 23:16:57 +0300

    Update vnc-xfce4.sh

--- vnc-xfce4.sh
+++ vnc-xfce4.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 sudo apt update
-sudo apt -y install xfce4 xfce4-goodies tightvncserver 
+sudo apt -y install xfce4 xfce4-goodies tightvncserver firefox
 vncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
 mv ./xstartup ~/.vnc/xstartup
 sudo chmod +x ~/.vnc/xstartup
 vncserver
-sudo apt-get install firefox
